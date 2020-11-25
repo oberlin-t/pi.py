b57d8bbd264959586005ea7d975512d90188df47
oberlin-t <thomas.l.oberlin@gmail.com>
2020-11-24 21:24:58 -0500

    switched to a 3d method of calculating

--- pi.py
+++ pi.py
@@ -1,19 +1,21 @@
 import random
+import math
 total = 0
-inCircle = 0
+inSphere = 0
 points = input("How many points do you want to compute? > ")
 
 for i in range(int(points)):
     x = random.uniform(0,10)
     y = random.uniform(0,10)
-    d = ((x-5)**2+(y-5)**2)**0.5
-
+    z = random.uniform(0,10)
+    d = math.sqrt(math.pow((x-5), 2) + math.pow((y-5), 2) + math.pow((z-5), 2))
+    
     if d <= 5:
-        inCircle += 1
+        inSphere += 1
         total += 1
     else:
         total += 1
 
-pi = ((inCircle / total) * 100) / 25 
+pi = ((inSphere / total) * 1000) / (500/3) 
 print("Pi =",pi)
 
