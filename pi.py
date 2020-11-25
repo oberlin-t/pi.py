b3db945a3b9d2c2417f8d699e5d0eca299204612
oberlin-t <thomas.l.oberlin@gmail.com>
2020-11-25 13:14:19 -0500

    rewrote to use circumfrence of the circle

--- pi.py
+++ pi.py
@@ -1,21 +1,17 @@
-import random
 import math
-total = 0
-inSphere = 0
-points = input("How many points do you want to compute? > ")
+lastx = 0.0
+lasty = 5.0
+dist = 0
 
-for i in range(int(points)):
-    x = random.uniform(0,10)
-    y = random.uniform(0,10)
-    z = random.uniform(0,10)
-    d = math.sqrt(math.pow((x-5), 2) + math.pow((y-5), 2) + math.pow((z-5), 2))
+prec = 3000000
+for i in range(prec * 5):
+    x = i / prec
+    y = math.sqrt(25-math.pow(x,2))
+    dist += math.sqrt(math.pow((x-lastx),2) + math.pow((y-lasty),2))
+    lastx = x
+    lasty = y
+
+dist += math.sqrt(math.pow((5-lastx),2) + math.pow((0-lasty),2))
     
-    if d <= 5:
-        inSphere += 1
-        total += 1
-    else:
-        total += 1
-
-pi = ((inSphere / total) * 1000) / (500/3) 
-print("Pi =",pi)
-
+pi = (dist * 4) / 10
+print(pi)
